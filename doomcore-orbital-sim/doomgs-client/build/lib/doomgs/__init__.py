54a0915cf89194ababaa4c66a466ee3acf69a80e
gorillablasters <chriscm.moore73@gmail.com>
2025-11-30 21:46:15 -0600

    removing build from repo

--- doomcore-orbital-sim/doomgs-client/build/lib/doomgs/__init__.py
+++
@@ -1,3 +0,0 @@
-from .client import DoomGSClient
-
-__all__ = ["DoomGSClient"]
--- doomcore-orbital-sim/doomgs-client/build/lib/doomgs/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def get_base_url() -> str:
-    """
-    Base URL for the backend FastAPI service.
-    Uses env DOOMGS_BASE_URL if set, otherwise defaults to localhost:8000.
-    """
-    return os.getenv("DOOMGS_BASE_URL", "http://localhost:8000")
